744ad37e4d47549a3931d842c200f33ac0d97162
PosikBoy <z.kalinichenko4@gmail.com>
2024-05-04 02:41:14 +0300

    delete folders

--- dist/services/priceService.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class PriceService {
-    generatePrice() {
-        return 1400;
-    }
-}
-const priceService = new PriceService();
-exports.default = priceService;
